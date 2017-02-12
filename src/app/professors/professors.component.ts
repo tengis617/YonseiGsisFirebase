f79b0a44d7dfc40753ff49d982f8eb2fc413b6c9
tengis617 <tengis617@gmail.com>
2017-02-12 12:21:58 +0900

    professors component initial

---
+++ src/app/professors/professors.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-professors',
+    templateUrl: 'professors.component.html'
+})
+export class ProfessorsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
