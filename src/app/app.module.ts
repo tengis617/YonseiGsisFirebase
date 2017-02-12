e9d611fa6a456ae4bf969a9cf794152c71b5c3b7
tengis617 <tengis617@gmail.com>
2017-02-12 12:19:00 +0900

    initial setup of firebase and core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// Angular Fire 2
+import { firebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from 'angularfire2';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app/app.component';
+import { NavComponent } from './core/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAuCb8ikGjE82Y-CRiRJJYfwYI4CufWv-0',
+    authDomain: 'yonseigsisfirebase.firebaseapp.com',
+    databaseURL: 'https://yonseigsisfirebase.firebaseio.com',
+    storageBucket: 'yonseigsisfirebase.appspot.com',
+    messagingSenderId: '867259797440',
+};
