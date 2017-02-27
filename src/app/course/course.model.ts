d29b461c1d858927a70381678f8005d1fb93352e
VargasAdrian <adrian.vargas92@gmail.com>
2017-02-27 17:13:40 +0900

    Nothing important
    Nothing important

--- src/app/course/course.model.ts
+++ src/app/course/course.model.ts
@@ -1,6 +1,7 @@
 import { IProfessor } from './../professor/professor.model';
 
 export interface ICourse {
+    id: Number;
     name: String;
     major: String;
     rating: Number;
--- src/app/professor/professor.model.ts
+++ src/app/professor/professor.model.ts
@@ -1,6 +1,7 @@
 import { ICourse } from './../course/course.model';
 
 export interface IProfessor { 
+    id: Number;
     name: String;
     courses: ICourse[];
     rating: Number;
